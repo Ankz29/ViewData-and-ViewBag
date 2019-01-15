266f560fd0575dcc9dfa9e1238cc2d2ff04355ad
Ankita <ankita.m@softwaysolutions.com>
2019-01-15 10:18:57 +0530

    POC on View data and View Bag

---
+++ Controllers/ViewDataViewBagController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewData_ViewBag.Models;
+
+namespace ViewData_ViewBag.Controllers
+{
+    public class ViewDataViewBagController : Controller
+    {
+        //
+        // GET: /ViewData_ViewBag/
+
+        public ActionResult Index()
+        {
+            List<ViewDataViewBag> model = new List<ViewDataViewBag>();
+            model.Add(new ViewDataViewBag()
+            {
+                Name = "ankita",
+                Address = "muddapur",
+                Phone = 9876543210
+            });
+            model.Add(new ViewDataViewBag()
+                {
+                    Name = "anisha",
+                    Address = "Bhosale",
+                    Phone = 487203858730,
+                });
+            model.Add(new ViewDataViewBag()
+            {
+                Name = "Bhaveshwari",
+                Address = "Bhambar",
+                Phone = 2364423747,
+            });
+            model.Add(new ViewDataViewBag()
+            {
+                Name = "Pooja",
+                Address = "Desai",
+                Phone = 747472782473,
+            });
+
+
+            ViewData["Details"] = model;
+            TempData["msg"] = "data is sent from temp data";
+
+            return View(ViewData["Details"]);
+            
+        }
+        
+    }
+}
---
+++ Models/ViewDataViewBag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ViewData_ViewBag.Models
+{
+    public class ViewDataViewBag
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public long Phone { get; set; }
+    }
+}
